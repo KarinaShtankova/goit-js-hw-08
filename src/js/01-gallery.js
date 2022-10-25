d249274564aa0b05ae61ab5c2f3c35a6cf0df582
KarinaShtankova <shtankovads@gmail.com>
2022-10-25 16:59:11 +0300

    add  1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryRef = document.querySelector(".gallery");
+
+const markup = galleryItems
+	.map(
+		({ preview, original, description }) => `
+
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+	)
+	.join("");
+
+galleryRef.insertAdjacentHTML("beforeend", markup);
+// console.log(markup);
+
+galleryRef.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt) {
+	evt.preventDefault();
+}
+
+const lightbox = new SimpleLightbox(".gallery a", { captionsData: `alt`, captionDelay: `250` });
