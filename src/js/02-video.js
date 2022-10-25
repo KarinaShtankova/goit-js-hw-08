3f2c829de2d9a5c046cd4585d808046a3c64dab0
KarinaShtankova <shtankovads@gmail.com>
2022-10-25 19:23:56 +0300

    add  task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+import VimeoPlayer from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(data) {
+  // data is an object containing properties specific to that event
+
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+  //   console.log(data.seconds);
+}
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
